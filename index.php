f20efd214804f27e09c74880de648b9ea9d70d59
sagarcb <sagar.cb.009@gmail.com>
2021-06-05 17:31:47 +0600

    Mastering Done

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include_once 'layouts/header.php'?>
+<?php include_once 'layouts/sidebar.php'?>
+
+    <!-- Page Content  -->
+    <div id="content" class="p-4 p-md-5 pt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Hello world</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include_once 'layouts/footer.php'?>
---
+++ layouts/sidebar.php
@@ -0,0 +1,24 @@
+
+<h1><a href="index.php" class="logo">Project Name</a></h1>
+<ul class="list-unstyled components mb-5">
+    <li class="active">
+        <a href="#"><span class="fa fa-home mr-3"></span> Homepage</a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-user mr-3"></span> Dashboard</a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-sticky-note mr-3"></span> Friends</a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-sticky-note mr-3"></span> Subcription</a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-paper-plane mr-3"></span> Settings</a>
+    </li>
+    <li>
+        <a href="#"><span class="fa fa-paper-plane mr-3"></span> Information</a>
+    </li>
+</ul>
+
+</nav>
